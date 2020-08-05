6b098b248588bc848c78b2cd6cc9526be048d7c0
Jordan Johnson <jordaninjohnson@gmail.com>
2020-08-05 16:08:00 -0600

    added homepage script

---
+++ client/build/precache-manifest.958e4fe3057354303f68908d32e7d3b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "97203913447cf70a4c11",
+    "url": "/static/css/main.a02b9b0a.chunk.css"
+  },
+  {
+    "revision": "97203913447cf70a4c11",
+    "url": "/static/js/main.492f0849.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "36deca38edc945d85b44",
+    "url": "/static/js/2.c4781635.chunk.js"
+  },
+  {
+    "revision": "157164397046de42a1fe8f7da602f419",
+    "url": "/index.html"
+  }
+];
